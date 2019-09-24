3fe1854da81f61cae01e51ef860d3cf668277e06
Wendler Eis <wendler_eis@hotmail.com>
2019-09-24 19:47:56 -0300

    feat: install eslint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
 routes.get('/', (req, res) => {
-  res.json({ ok: false })
-})
+  res.json({ ok: false });
+});
 
-export default routes
+export default routes;
