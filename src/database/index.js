799288a0e318bf1bb462fba82dde8af25b216b86
Wendler Eis <wendler_eis@hotmail.com>
2019-09-25 18:58:21 -0300

    feat: add database connection

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+import Sequelize from 'sequelize';
+
+import databaseConfig from '../config/database';
+
+import User from '../app/models/User';
+
+const models = [User];
+
+export default class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
