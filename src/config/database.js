7def18fd368fcf7219777fa505b3310c3f77d54f
Wendler Eis <wendler_eis@hotmail.com>
2019-09-24 23:45:07 -0300

    feat: install sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'pg',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gobarber',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
